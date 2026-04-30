c2159a228ec4e332ba8cbf82b7538cce92531244
kurutis <cristina4n@yandex.ru>
2026-04-30 07:18:51 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -40,7 +40,65 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'eeilkqxacnrkriitimjs.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.selcdn.ru',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.s3.ru-7.storage.selcloud.ru',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http', 
+        hostname: '*.s3.ru-7.storage.selcloud.ru',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '30bd5b8c-136d-48e3-b7c1-71a168d4fef4.selstorage.ru',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.selstorage.ru',
+        port: '',
+        pathname: '/**',
+      },
     ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ['image/webp'],
+    minimumCacheTTL: 60,
+  },
+  serverExternalPackages: ['pg', '@neondatabase/serverless'],
+  
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
   },
 };
 
