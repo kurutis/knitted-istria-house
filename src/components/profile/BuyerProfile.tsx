fc7ce0dcd18cc0611fb329c782480ff3b3608b2c
kurutis <cristina4n@yandex.ru>
2026-05-22 22:13:30 +0300

    das

--- src/components/profile/BuyerProfile.tsx
+++ src/components/profile/BuyerProfile.tsx
@@ -452,12 +452,12 @@ export default function BuyerProfile({
                       <h2 className="font-['Montserrat_Alternates'] font-semibold text-2xl mb-6 bg-linear-to-r from-firm-orange to-firm-pink bg-clip-text text-transparent flex items-center gap-2"><ProductsIcon color="#F4A67F" className="w-6 h-6" />Мои заказы</h2>
                       
                       {orders.length === 0 ? (
-                        <div className="text-center py-12 bg-main rounded-xl flex gap-2">
+                        <div className="text-center py-12 bg-main rounded-xl">
                           <div className="mb-4">
                             <ProductsIcon color="#D97C8E" className="w-16 h-16 mx-auto opacity-50" />
                           </div>
                           <p className="text-firm-gray mb-4 font-['Montserrat_Alternates']">У вас пока нет заказов</p>
-                          <Link href="/catalog" className="inline-block px-6 py-3 bg-linear-to-r from-firm-orange to-firm-pink text-main rounded-xl hover:shadow-lg transition-all"><CatalogPinkIcon color="#f9f9f9" className="w-5 h-5" /> Перейти в каталог</Link>
+                          <Link href="/catalog" className="flex gap-2 px-6 py-3 bg-linear-to-r from-firm-orange to-firm-pink text-main rounded-xl hover:shadow-lg transition-all"><CatalogPinkIcon color="#f9f9f9" className="w-5 h-5" /> Перейти в каталог</Link>
                         </div>
                       ) : (
                         <div className="space-y-4">
