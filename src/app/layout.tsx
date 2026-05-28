787b45b7aab1802ba2617bba780d0f91a965c465
kurutis <cristina4n@yandex.ru>
2026-05-29 02:33:33 +0300

    фыв

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,60 +1,20 @@
 import type { Metadata } from "next"
-import localFont from "next/font/local"
+import { Raleway, Montserrat_Alternates } from "next/font/google"
 import "./globals.css"
 import { Providers } from "./providers"
 import AppLayout from "@/components/layout/AppLayout"
 import ErrorHandler from "@/components/ErrorHandler"
 
-const raleway = localFont({
-    src: [
-        {
-            path: "../fonts/Raleway/static/Raleway-Regular.ttf",
-            weight: "400",
-            style: "normal",
-        },
-        {
-            path: "../fonts/Raleway/static/Raleway-Medium.ttf",
-            weight: "500",
-            style: "normal",
-        },
-        {
-            path: "../fonts/Raleway/static/Raleway-SemiBold.ttf",
-            weight: "600",
-            style: "normal",
-        },
-        {
-            path: "../fonts/Raleway/static/Raleway-Bold.ttf",
-            weight: "700",
-            style: "normal",
-        },
-    ],
-    variable: "--font-raleway",
+const raleway = Raleway({
+    subsets: ['cyrillic', 'latin'],
+    weight: ['400', '500', '600', '700'],
+    variable: '--font-raleway',
 })
 
-const montserratAlternates = localFont({
-    src: [
-        {
-            path: "../fonts/Montserrat_Alternates/MontserratAlternates-Regular.ttf",
-            weight: "400",
-            style: "normal",
-        },
-        {
-            path: "../fonts/Montserrat_Alternates/MontserratAlternates-Medium.ttf",
-            weight: "500",
-            style: "normal",
-        },
-        {
-            path: "../fonts/Montserrat_Alternates/MontserratAlternates-SemiBold.ttf",
-            weight: "600",
-            style: "normal",
-        },
-        {
-            path: "../fonts/Montserrat_Alternates/MontserratAlternates-Bold.ttf",
-            weight: "700",
-            style: "normal",
-        },
-    ],
-    variable: "--font-montserrat-alternates",
+const montserratAlternates = Montserrat_Alternates({
+    subsets: ['cyrillic', 'latin'],
+    weight: ['400', '500', '600', '700'],
+    variable: '--font-montserrat-alternates',
 })
 
 export const metadata: Metadata = { 
@@ -65,7 +25,7 @@ export const metadata: Metadata = {
     },
 };
 
-export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
+export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
     return (
         <html lang="ru">
             <body className={`${raleway.variable} ${montserratAlternates.variable}`}>
