f188ee2eba8df0b3f769f0d03ca2c646d2196d89
kurutis <andreevvakristina@gmail.com>
2026-06-06 02:12:19 +0300

    sdfghjk

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -221,7 +221,7 @@ function CatalogContent() {
     <div className="min-h-screen bg-main">
       <div className="container mx-auto px-3 sm:px-4 py-6 sm:py-8">
         <div className="mb-6 sm:mb-8">
-          <h1 className="font-['Montserrat_Alternates'] font-semibold text-2xl sm:text-3xl lg:text-4xl bg-gradient-to-r from-firm-orange to-firm-pink bg-clip-text text-transparent">
+          <h1 className="font-['Montserrat_Alternates'] font-semibold text-2xl sm:text-3xl lg:text-4xl bg-linear-to-r from-firm-orange to-firm-pink bg-clip-text text-transparent !important">
             Каталог изделий
           </h1>
           <p className="text-firm-gray mt-2 text-sm">{pagination.total} уникальных изделий ручной работы</p>
