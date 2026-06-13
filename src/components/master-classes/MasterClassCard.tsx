065ba780a8a4142b6a99cee79ac5ad06cc3c454f
kurutis <andreevvakristina@gmail.com>
2026-06-13 20:53:38 +0300

    фывфы

--- src/components/master-classes/MasterClassCard.tsx
+++ src/components/master-classes/MasterClassCard.tsx
@@ -149,11 +149,15 @@ export default function MasterClassCard({masterClass, session, onRegister, onCan
                         ) : isRegistered ? (
                             <div className="flex gap-2">
                                 {masterClass.type === 'online' && masterClass.online_link && (<motion.button onClick={() => window.open(masterClass.online_link, '_blank')} className="flex items-center gap-1 px-3 py-1.5 bg-firm-green text-main rounded-lg text-sm hover:bg-green-400 transition" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} transition={{ duration: 0.15 }}><LaunchIcon /><span className='text-main'>Запустить</span></motion.button>)}
-                               <motion.button className="group flex items-center gap-1 px-3 py-1.5 border border-firm-red text-firm-red rounded-lg text-sm hover:bg-firm-red transition-all duration-300">
-  <span className="[&_svg_path]:stroke-firm-red group-hover:[&_svg_path]:stroke-main">
-    <CancelIcon color="#D77C7C" size={14} />
-  </span>
-  <span className="group-hover:text-main">Отменить</span>
+                               <motion.button
+  onClick={() => onCancel(masterClass.id)}
+  className="group flex items-center gap-1 px-3 py-1.5 border border-firm-red text-firm-red rounded-lg text-sm hover:bg-firm-red hover:text-main transition-all duration-300"
+  whileHover={{ scale: 1.05 }}
+  whileTap={{ scale: 0.95 }}
+  transition={{ duration: 0.15 }}
+>
+  <CancelIcon className="w-3.5 h-3.5 stroke-firm-red group-hover:stroke-main" size={14} />
+  <span>Отменить</span> {/* цвет унаследует от кнопки */}
 </motion.button>
                             </div>
                         ) : (
